e8e056e4e443b0af007bf675abed4eb78d3f0866
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-01-31 16:22:23 -0300

    ✨feat: criando uma task para gerar a documentação no final da pipe

--- prefect/teste.py
+++ prefect/teste.py
@@ -23,11 +23,21 @@ def run_dbt_models():
     )
     
     return True
+
+@task
+def create_docs():
     
+    subprocess.run(
+        ["dbt", "docs", "generate","--project-dir", "./dbt/learning/"]
+    )
+    
+    return True
+
 @flow
 def etl():
     result = save_csv_as_table()
     result2 = run_dbt_models()
-    print(result, result2)
+    result3 = create_docs()
+    print(result, result2, result3)
     
 etl()
